484142b0100a8b2aa723a4866730a81ad86dcc9e
dezso <serban.dezso.david.97.07.08@gmail.com>
2025-01-17 13:33:12 +0100

    a magyar nyelvben a megnevezés javítása

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,12 +1,11 @@
 export const environment = {
   production:true,
   firebaseConfig : {
-    apiKey: "AIzaSyAn6137bTdBEuEYlt_UTOd0u6fc7QnatyY",
-    authDomain: "dolgozat2025-01.firebaseapp.com",
-    projectId: "dolgozat2025-01",
-    databaseURL: "https://dolgozat2025-01-default-rtdb.europe-west1.firebasedatabase.app",
-    storageBucket: "dolgozat2025-01.firebasestorage.app",
-    messagingSenderId: "443818520671",
-    appId: "1:443818520671:web:3881180d85e3b644416bea"
+    apiKey: "AIzaSyAmlcOounhaBd9fMyFjs1oM2NJNaSpDuPs",
+    authDomain: "frontenddolgozat2025-01-17.firebaseapp.com",
+    projectId: "frontenddolgozat2025-01-17",
+    storageBucket: "frontenddolgozat2025-01-17.firebasestorage.app",
+    messagingSenderId: "55793260465",
+    appId: "1:55793260465:web:3995cc2b624d8eb736cf90"
   }
 };
